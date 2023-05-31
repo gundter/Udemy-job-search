84f57bcb7ffee2cd9dcd1b1aa203158c7732d22e
Terry Gunderson <tgunderson25@gmail.com>
2023-05-31 15:10:52 -0500

    Complete Section 9

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         'brand-gray-1': '#DADCE0',
         'brand-blue-1': '#1967D2',
         'brand-green-1': '#137333'
+      },
+      boxShadow: {
+        blue: '0 0 3px 3px #4285F4'
       }
     }
   },
--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/vue';
+import userEvent from '@testing-library/user-event';
 import MainNav from '@/components/MainNav.vue';
 
 describe('MainNav', () => {
@@ -20,5 +21,33 @@ describe('MainNav', () => {
       'Students',
       'Jobs'
     ]);
+  });
+
+  describe('When the user logs in', () => {
+    it('displays user profile image', async () => {
+      render(MainNav);
+
+      let profileImage = screen.queryByRole('img', {
+        name: /user profile image/i
+      });
+      expect(profileImage).not.toBeInTheDocument();
+
+      let logInButton = screen.queryByRole('button', {
+        name: /sign in/i
+      });
+      expect(logInButton).toBeInTheDocument();
+
+      await userEvent.click(logInButton);
+
+      profileImage = screen.getByRole('img', {
+        name: /user profile image/i
+      });
+      logInButton = screen.queryByRole('button', {
+        name: /sign in/i
+      });
+
+      expect(profileImage).toBeInTheDocument();
+      expect(logInButton).not.toBeInTheDocument();
+    });
   });
 });
